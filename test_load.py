9d70ac3440d898577e5913f7637d683cc5b53ce5
Fraser Greenlee <fraser.greenlee@skyscanner.net>
2020-12-08 08:52:10 +0000

    test uploaded dataset

--- test_load.py
+++ test_load.py
@@ -3,3 +3,7 @@ import datasets
 data = datasets.load_dataset('Fraser/news-category-dataset')
 
 print(data['train'][0])
+
+data = datasets.load_dataset('Fraser/python-lines')
+
+print(data['train'][0])
